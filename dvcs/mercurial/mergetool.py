5fa255bec5140e5af031db0ab611da5365254bdc
vlinhart <vladimir.linhart@gmail.com>
2012-03-19 17:46:17 +0100

    don't read files in merge tool

--- dvcs/mercurial/mergetool.py
+++ dvcs/mercurial/mergetool.py
@@ -9,16 +9,15 @@
 """
 
 import sys
-from utils import read_file
 
 try:
     import simplejson as json
 except ImportError:
     import json
 
-base = read_file(sys.argv[1])
-local = read_file(sys.argv[2])
-other = read_file(sys.argv[3])
+base = sys.argv[1]
+local = sys.argv[2]
+other = sys.argv[3]
 tar = sys.argv[4]
 
 out = {'base': base, 'local': local, 'other': other, 'tar': tar}
